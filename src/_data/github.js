a6cf9c0e676bb8007fdced700e072f727f680062
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2022-11-11 09:12:46 +0100

    Fixat github_user tror jag

--- src/_data/github.js
+++ src/_data/github.js
@@ -2,11 +2,11 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 require('dotenv').config();
 
 module.exports = async function() {
-    const githubUser = process.env.GITHUB_USER;
-    if (!githubUser) {
-        throw new Error('Please set a GITHUB_USER environment variable');
-    }
-    let url = `https://api.github.com/users/${githubUser}`;
+    //const githubUser = process.env.GITHUB_USER;
+   // if (!githubUser) {
+     //   throw new Error('Please set a GITHUB_USER environment variable');
+   // }
+   let url = `https://api.github.com/users/TimNilsson04`;
 
     /* This returns a promise */
     return EleventyFetch(url, {
